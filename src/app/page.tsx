c7b900d7215248de4f5668582cd8ae5985869bde
Nikita Grebnev <ocelloid@gmail.com>
2025-02-28 15:31:48 +0500

    animation and zoom control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,31 @@ import { Canvas } from "@react-three/fiber";
 import { Mars } from "./_components/canvas";
 import { PointLight } from "three";
 import { OrbitControls } from "@react-three/drei";
+import { Environment, Text } from "@react-three/drei";
+import { Suspense, useEffect, useState } from "react";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+
+const CameraAnimation = () => {
+  const [animate, setAnimate] = useState(false);
+  const vec = new THREE.Vector3(1, 1, -1);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
+  useFrame((state) => {
+    if (animate) {
+      state.camera.lookAt(0, 0, 0);
+      state.camera.position.lerp(vec, 0.016);
+      if (state.camera.position.distanceTo(vec) < 0.1) {
+        setAnimate(false);
+      }
+    }
+    return null;
+  });
+  return null;
+};
 
 export default function Home() {
   const light = new PointLight();
@@ -18,8 +43,27 @@ export default function Home() {
           scene.add(camera);
         }}
       >
-        <OrbitControls />
-        <Mars />
+        <Suspense fallback={<Text>Loading...</Text>}>
+          <CameraAnimation />
+          <Environment
+            files={"/textures/environment.hdr"}
+            background
+            backgroundBlurriness={0.025}
+          />
+          <OrbitControls
+            onChange={(e) => {
+              const cam = e?.target?.object;
+              console.log(cam?.position.distanceTo(new THREE.Vector3(0, 0, 0)));
+              if (
+                cam &&
+                cam.position.distanceTo(new THREE.Vector3(0, 0, 0)) < 1.1
+              ) {
+                cam.position.set(0.66, 0.66, -0.66);
+              }
+            }}
+          />
+          <Mars />
+        </Suspense>
       </Canvas>
     </main>
   );
