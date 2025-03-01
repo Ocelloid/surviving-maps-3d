04a570b122778db82be3d3118613dba1e55117e6
Nikita Grebnev <ocelloid@gmail.com>
2025-03-01 15:17:01 +0500

    db schema

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,118 +1,106 @@
 "use client";
-import Papa from "papaparse";
-import { useEffect, useState } from "react";
+// import Papa from "papaparse";
+// import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 
-type Row = {
-  Altitude: string;
-  "Breakthrough 1": string;
-  "Breakthrough 2": string;
-  "Breakthrough 3": string;
-  "Breakthrough 4": string;
-  "Breakthrough 5": string;
-  "Breakthrough 6": string;
-  "Breakthrough 7": string;
-  "Breakthrough 8": string;
-  "Breakthrough 9": string;
-  "Breakthrough 10": string;
-  "Breakthrough 11": string;
-  "Breakthrough 12": string;
-  "Cold Waves": string;
-  Concrete: string;
-  "Difficulty Challenge": string;
-  "Dust Devils": string;
-  "Dust Storms": string;
-  Latitude: string;
-  "Latitude °": string;
-  Longitude: string;
-  "Longitude °": string;
-  "Map Name": string;
-  Metals: string;
-  Meteors: string;
-  "Named Location": string;
-  "Rare Metals": string;
-  Temperature: string;
-  Topography: string;
-  Water: string;
-};
+// type Row = {
+//   Altitude: string;
+//   "Breakthrough 1": string;
+//   "Breakthrough 2": string;
+//   "Breakthrough 3": string;
+//   "Breakthrough 4": string;
+//   "Breakthrough 5": string;
+//   "Breakthrough 6": string;
+//   "Breakthrough 7": string;
+//   "Breakthrough 8": string;
+//   "Breakthrough 9": string;
+//   "Breakthrough 10": string;
+//   "Breakthrough 11": string;
+//   "Breakthrough 12": string;
+//   "Breakthrough 13"?: string;
+//   "Breakthrough 14"?: string;
+//   "Breakthrough 15"?: string;
+//   "Breakthrough 16"?: string;
+//   "Breakthrough 17"?: string;
+//   "Cold Waves": string;
+//   Concrete: string;
+//   "Difficulty Challenge": string;
+//   "Dust Devils": string;
+//   "Dust Storms": string;
+//   Latitude: string;
+//   "Latitude °": string;
+//   Longitude: string;
+//   "Longitude °": string;
+//   "Map Name": string;
+//   Metals: string;
+//   Meteors: string;
+//   "Named Location": string;
+//   "Rare Metals": string;
+//   Temperature: string;
+//   Topography: string;
+//   Water: string;
+// };
 
 export default function Admin() {
-  const [isParsing, setIsParsing] = useState(false);
-  const [data, setData] = useState<Row[]>([]);
+  // const [isParsing, setIsParsing] = useState(false);
+  // const [evansGP, setEvansGP] = useState<Row[]>([]);
+  // const [picardBB, setPicardBB] = useState<Row[]>([]);
+  // const [picardGPBB, setPicardGPBB] = useState<Row[]>([]);
+  // const [picardGP, setPicardGP] = useState<Row[]>([]);
+  // const [picard, setPicard] = useState<Row[]>([]);
+  // const [titoGP, setTitoGP] = useState<Row[]>([]);
+  // const [log, setLog] = useState<string[]>([]);
 
-  async function fetchCSV() {
-    setIsParsing(true);
-    const response = await fetch("mapdata/Trim.csv");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.text();
-    const rows: Row[] = [];
-    Papa.parse(data, {
-      worker: true,
-      header: true,
-      step: function (results) {
-        const row = results.data as Row;
-        console.log(row);
-        rows.push(row);
-      },
-      complete: function () {
-        console.log("CSV file successfully processed");
-        setData(rows);
-      },
-      error: function () {
-        console.error("Error parsing CSV");
-      },
-    });
-    setIsParsing(false);
-  }
+  // async function fetchCSV(
+  //   file: string,
+  //   stateCallback?: Dispatch<SetStateAction<Row[]>>,
+  // ) {
+  //   setIsParsing(true);
+  //   const response = await fetch(file);
+  //   const data = await response.text();
+  //   const rows: Row[] = [];
+  //   Papa.parse(data, {
+  //     worker: true,
+  //     header: true,
+  //     step: function (results) {
+  //       const row = results.data as Row;
+  //       rows.push(row);
+  //     },
+  //     complete: function () {
+  //       stateCallback?.(rows);
+  //     },
+  //   });
+  //   setIsParsing(false);
+  // }
 
-  useEffect(() => {
-    void fetchCSV();
-  }, []);
+  // useEffect(() => {
+  //   void fetchCSV("/mapdata/Evans_GP.csv", setEvansGP);
+  //   void fetchCSV("/mapdata/Picard_BB.csv", setPicardBB);
+  //   void fetchCSV("/mapdata/Picard_GP_BB.csv", setPicardGPBB);
+  //   void fetchCSV("/mapdata/Picard_GP.csv", setPicardGP);
+  //   void fetchCSV("/mapdata/Picard.csv", setPicard);
+  //   void fetchCSV("/mapdata/Tito_GP.csv", setTitoGP);
+  // }, []);
 
-  return (
-    <div className="flex flex-col items-center justify-center">
-      {isParsing ? (
-        <div className="flex flex-col items-center justify-center">
-          <p>Parsing...</p>
-        </div>
-      ) : (
-        <div className="flex w-full flex-col">
-          <p>Data</p>
-          <div className="flex w-full flex-col">
-            {data.map((row, index) => (
-              <div key={index} className="flex flex-row items-start">
-                {row.Longitude} {row["Longitude °"]} {row.Latitude}{" "}
-                {row["Latitude °"]}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      {/* <p>mapdata</p>
-      Latitude °,Latitude,Longitude °,Longitude,Topography,Difficulty
-      Challenge,Altitude,Temperature,Metals,Rare Metals,Concrete,Water,Dust
-      Devils,Dust Storms,Meteors,Cold Waves,Map Name,Named Location,Breakthrough
-      1,Breakthrough 2,Breakthrough 3,Breakthrough 4,Breakthrough 5,Breakthrough
-      6,Breakthrough 7,Breakthrough 8,Breakthrough 9,Breakthrough
-      10,Breakthrough 11,Breakthrough 12,Breakthrough 13,Breakthrough
-      14,Breakthrough 15,Breakthrough 16,Breakthrough 17
-      <br />
-      <br />
-      <p>bt_desc</p>
-      name_en,name_br,name_fr,name_ge,name_po,name_ru,name_sc,name_sp
-      <br />
-      <br />
-      <p>bt</p>
-      name_en,name_br,name_fr,name_ge,name_po,name_ru,name_sc,name_sp
-      <br />
-      <br />
-      <p>locations</p>
-      name_en,name_br,name_fr,name_ge,name_po,name_ru,name_sc,name_sp
-      <br />
-      <br />
-      <p>misc</p>
-      name_en,name_br,name_fr,name_ge,name_po,name_ru,name_sc,name_sp */}
-    </div>
-  );
+  // useEffect(() => {
+  //   if (
+  //     !!evansGP.length &&
+  //     !!picardBB.length &&
+  //     !!picardGPBB.length &&
+  //     !!picardGP.length &&
+  //     !!picard.length &&
+  //     !!titoGP.length
+  //   ) {
+  //     setLog((prev) => [...prev, evansGP.length + " Evans GP maps loaded"]);
+  //     setLog((prev) => [...prev, picardBB.length + " Picard BB maps loaded"]);
+  //     setLog((prev) => [
+  //       ...prev,
+  //       picardGPBB.length + " Picard GP BB maps loaded",
+  //     ]);
+  //     setLog((prev) => [...prev, picardGP.length + " Picard GP maps loaded"]);
+  //     setLog((prev) => [...prev, picard.length + " Picard maps loaded"]);
+  //     setLog((prev) => [...prev, titoGP.length + " Tito GP maps loaded"]);
+  //   }
+  // }, [evansGP, picardBB, picardGPBB, picardGP, picard, titoGP]);
+
+  return <div className="flex flex-col items-center justify-center"></div>;
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -16,27 +16,109 @@ import { type AdapterAccount } from "next-auth/adapters";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `surviving-maps-3d_${name}`);
+export const createTable = pgTableCreator(
+  (name) => `surviving-maps-3d_${name}`,
+);
 
-export const posts = createTable(
-  "post",
+export const locations = createTable("location", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  lat_dir: varchar("lat_dir", { length: 1 }),
+  lat_deg: varchar("lat_deg", { length: 3 }),
+  lon_dir: varchar("lon_dir", { length: 1 }),
+  lon_deg: varchar("lon_deg", { length: 3 }),
+  altitude: integer("altitude"),
+  named_loc_id: integer("named_loc_id"),
+  map_name: varchar("map_name", { length: 256 }),
+  topography: varchar("topography", { length: 256 }),
+  concrete: integer("concrete"),
+  water: integer("water"),
+  metals: integer("metals"),
+  rare_metals: integer("rare_metals"),
+  temperature: integer("temperature"),
+  meteors: integer("meteors"),
+  dust_devils: integer("dust_devils"),
+  dust_storms: integer("dust_storms"),
+  cold_waves: integer("cold_waves"),
+  difficulty: integer("difficulty"),
+});
+
+export const locationsRelations = relations(locations, ({ one }) => ({
+  namedLoc: one(namedLocations, {
+    fields: [locations.named_loc_id],
+    references: [namedLocations.id],
+  }),
+}));
+
+export const namedLocations = createTable("named_location", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  name_en: varchar("name_en", { length: 256 }),
+  name_br: varchar("name_br", { length: 256 }),
+  name_fr: varchar("name_fr", { length: 256 }),
+  name_ge: varchar("name_ge", { length: 256 }),
+  name_po: varchar("name_po", { length: 256 }),
+  name_ru: varchar("name_ru", { length: 256 }),
+  name_sc: varchar("name_sc", { length: 256 }),
+  name_sp: varchar("name_sp", { length: 256 }),
+});
+
+export const namedLocationsRelations = relations(
+  namedLocations,
+  ({ many }) => ({
+    coordinates: many(locations),
+  }),
+);
+
+export const breakthroughs = createTable("breakthrough", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  name_en: varchar("name_en", { length: 256 }),
+  name_br: varchar("name_br", { length: 256 }),
+  name_fr: varchar("name_fr", { length: 256 }),
+  name_ge: varchar("name_ge", { length: 256 }),
+  name_po: varchar("name_po", { length: 256 }),
+  name_ru: varchar("name_ru", { length: 256 }),
+  name_sc: varchar("name_sc", { length: 256 }),
+  name_sp: varchar("name_sp", { length: 256 }),
+  desc_en: text("desc_en"),
+  desc_br: text("desc_br"),
+  desc_fr: text("desc_fr"),
+  desc_ge: text("desc_ge"),
+  desc_po: text("desc_po"),
+  desc_ru: text("desc_ru"),
+  desc_sc: text("desc_sc"),
+  desc_sp: text("desc_sp"),
+});
+
+export const versions = createTable("version", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  name: varchar("name", { length: 256 }),
+});
+
+export const breakthroughsInLocations = createTable(
+  "breakthrough_in_location",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("created_by", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
+    bt_id: integer("bt_id").notNull(),
+    loc_id: integer("loc_id").notNull(),
+    ver_id: integer("ver_id").notNull(),
   },
-  (example) => ({
-    createdByIdIdx: index("created_by_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  })
+);
+
+export const breakthroughsInLocationsRelations = relations(
+  breakthroughsInLocations,
+  ({ one }) => ({
+    bt: one(breakthroughs, {
+      fields: [breakthroughsInLocations.bt_id],
+      references: [breakthroughs.id],
+    }),
+    loc: one(locations, {
+      fields: [breakthroughsInLocations.loc_id],
+      references: [locations.id],
+    }),
+    ver: one(versions, {
+      fields: [breakthroughsInLocations.ver_id],
+      references: [versions.id],
+    }),
+  }),
 );
 
 export const users = createTable("user", {
@@ -83,7 +165,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_user_id_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -106,7 +188,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_user_id_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -125,5 +207,5 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
