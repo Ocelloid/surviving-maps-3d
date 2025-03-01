49cc5bda261838a2bbc2a43f68d9b5429ced98b9
Nikita Grebnev <ocelloid@gmail.com>
2025-03-02 00:58:18 +0500

    more styles

--- src/app/_components/ui/Menu.tsx
+++ src/app/_components/ui/Menu.tsx
@@ -4,6 +4,19 @@ import { useStore } from "~/store";
 import Image from "next/image";
 import IconLoading from "./IconLoading";
 
+const Rhombi = ({ value }: { value: number }) => {
+  return (
+    <div className="flex w-min flex-row gap-0.5">
+      {Array.from({ length: value }).map((_, i) => (
+        <div className="h-4 w-4 -skew-x-12 rounded-sm border-1 border-indigo-200 bg-indigo-400/75" />
+      ))}
+      {Array.from({ length: 4 - value }).map((_, i) => (
+        <div className="h-4 w-4 -skew-x-12 rounded-sm border-1 border-indigo-200 bg-indigo-950" />
+      ))}
+    </div>
+  );
+};
+
 export default function Menu() {
   const { locData, locationLoading } = useStore();
   return (
@@ -19,102 +32,78 @@ export default function Menu() {
             {locData?.lat_dir} {locData?.lat_deg} {locData?.lon_dir}{" "}
             {locData?.lon_deg}
           </p>
-          <div className="flex flex-row justify-between text-2xl">
-            <div className="flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/metals.png"}
-                alt="metals"
-                width={32}
-                height={32}
-              />
-              {locData?.metals}
+          <div className="gap-1">
+            <p className="-mt-4 text-2xl text-yellow-400">
+              {locData?.namedLoc?.name_en ?? "Unknown Location"}
+            </p>
+            <div className="flex flex-row justify-between">
+              <p className="text-blue-300">Difficulty Challenge</p>
+              {locData?.difficulty}
             </div>
-            <div className="flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/rare_metals.png"}
-                alt="rare_metals"
-                width={32}
-                height={32}
-              />
-              {locData?.rare_metals}
+            <div className="flex flex-row justify-between">
+              <p className="text-blue-300">Average Altitude</p>{" "}
+              {locData?.altitude} m
             </div>
-            <div className="flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/concrete.png"}
-                alt="concrete"
-                width={32}
-                height={32}
-              />
-              {locData?.concrete}
+            <div className="flex flex-row justify-between">
+              <p className="text-blue-300">Mean Temperature</p>
+              {locData?.temperature}
+              {"°C"}
             </div>
-            <div className="flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/water.png"}
-                alt="water"
-                width={32}
-                height={32}
-              />
-              {locData?.water}
+            <div className="flex flex-row justify-between">
+              <p className="text-blue-300">Topography</p> {locData?.topography}
             </div>
           </div>
-          <div className="text-outline flex flex-row justify-between text-2xl">
-            <div className="relative flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/storm.png"}
-                alt="storms"
-                width={64}
-                height={32}
-              />
-              <p className="absolute left-[25px] top-0">
-                {locData?.dust_storms}
-              </p>
+          <div className="grid grid-cols-2 gap-2 text-xs">
+            <p className="text-2xl uppercase text-blue-300">THREATS</p>
+            <p className="text-2xl uppercase text-blue-300">RESOURCES</p>
+            <div className="flex flex-col gap-2">
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.dust_devils ?? 0} />
+                <p>Dust Devils</p>
+              </div>
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.dust_storms ?? 0} />
+                <p>Dust Storms</p>
+              </div>
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.concrete ?? 0} />
+                <p>Meteors</p>
+              </div>
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.water ?? 0} />
+                <p>Cold Waves</p>
+              </div>
             </div>
-            <div className="relative flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/devil.png"}
-                alt="devils"
-                width={64}
-                height={32}
-              />
-              <p className="absolute left-[25px] top-0">
-                {locData?.dust_devils}
-              </p>
-            </div>
-            <div className="relative flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/cold.png"}
-                alt="cold"
-                width={64}
-                height={32}
-              />
-              <p className="absolute left-[25px] top-0">
-                {locData?.cold_waves}
-              </p>
-            </div>
-            <div className="relative flex flex-row items-center gap-2">
-              <Image
-                src={"/icons/meteors.png"}
-                alt="meteors"
-                width={64}
-                height={32}
-              />
-              <p className="absolute left-[25px] top-0">{locData?.meteors}</p>
+            <div className="flex flex-col gap-2">
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.metals ?? 0} />
+                <p>Metals</p>
+              </div>
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.rare_metals ?? 0} />
+                <p>Rare Metals</p>
+              </div>
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.concrete ?? 0} />
+                <p>Concrete</p>
+              </div>
+              <div className="grid grid-cols-2 items-center">
+                <Rhombi value={locData?.water ?? 0} />
+                <p>Water</p>
+              </div>
             </div>
           </div>
           <Image
             src={`/topology/${locData?.map_name}.png`}
-            className="bevel-clip-sm rounded-tl-2xl"
+            className="bevel-clip-sm mt-2 rounded-tl-2xl"
             alt="topology"
             width={480}
             height={270}
           />
-          <p className="-mt-2 ml-auto text-xs">Map Name: {locData?.map_name}</p>
-          <p className="text-2xl text-yellow-400">
-            {locData?.namedLoc?.name_en}
+          <p className="-mt-2 ml-auto text-xs text-blue-300">
+            {locData?.map_name}
           </p>
-          <p>Difficulty Challenge: {locData?.difficulty}</p>
-          <p>Temperature: {locData?.temperature}</p>
-          <p className="mt-2 text-2xl">Breakthroughs</p>
+          <p className="text-2xl uppercase text-blue-300">Breakthroughs</p>
           <div className="flex flex-col gap-4">
             {locData?.bts_loc
               ?.filter((btsloc) => btsloc.ver_id === 1)
