7ea41527a62f16c02df49bdc19abe2769a34f100
Nikita Grebnev <ocelloid@gmail.com>
2025-03-04 02:39:42 +0500

    additional padding for mobile

--- src/app/_components/ui/UI.tsx
+++ src/app/_components/ui/UI.tsx
@@ -4,7 +4,7 @@ import LocationDetails from "./LocationDetails";
 
 export default function UI() {
   return (
-    <div className="flex h-full min-h-dvh w-full flex-col gap-3 p-3 lg:flex-row lg:justify-between">
+    <div className="flex h-full min-h-dvh w-full flex-col gap-3 p-3 pb-12 lg:flex-row lg:justify-between lg:pb-0">
       <Filter />
       <LocationsList />
       <LocationDetails />
