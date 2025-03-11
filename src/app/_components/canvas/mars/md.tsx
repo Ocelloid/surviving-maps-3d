4db9b831e3519491fe2adf98f60d34e5de70c05c
Nikita Grebnev <ocelloid@gmail.com>
2025-03-11 20:50:49 +0500

    very slight normal map changes

--- src/app/_components/canvas/mars/md.tsx
+++ src/app/_components/canvas/mars/md.tsx
@@ -3,13 +3,13 @@ import { Sphere, useTexture } from "@react-three/drei";
 
 export default function MarsMD() {
   const colorTexture = useTexture("/textures/mars_4k_color.jpg");
-  const normalTexture = useTexture("/textures/mars_4k_normal.jpg");
+  const normalTexture = useTexture("/textures/mars_6k_normal.jpg");
   return (
     <Sphere>
       <meshStandardMaterial
         map={colorTexture}
         normalMap={normalTexture}
-        normalScale={[4, 4]}
+        normalScale={[6, 2]}
       />
       <sphereGeometry args={[1, 512, 512]} />
     </Sphere>
